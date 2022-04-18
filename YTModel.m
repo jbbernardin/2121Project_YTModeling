da98bcfd50af1336a40860a44dc8678fe3126851
jbbernardin <tuk59920@temple.edu>
2022-04-17 22:12:36 -0400

    Finished the basic model
    I have the basic model working how I want/expect. Now I need to have a variation that creates a histogram for each of the 5 stats, have a variation for variable investigation, comment, and save figures.

--- YTModel.m
+++ YTModel.m
@@ -10,21 +10,29 @@ function YTModel
 
 % 02/2022 by John Bernardin
 
+tic
 
 % Data Tracking
 total_views = 0; 
+total_likes = 0;
+total_dislikes = 0;
+total_comments = 0; % total comments
 total_subs = 0; % total subscribers
 
 % Basic Statistics
-Ndays = 1000; % total uploads (assuming one video per day)
-Nusers = 122000; % daily users (from sources it is 122000000, needed to reduce it for runtime)
+Ndays = 52; % number of days
+Nusers = 10000000; % daily users (from sources it is 122000000, needed to reduce it for runtime since even at 5Ndays, it would take 5 hours)
 hours_uploaded_every_minute = 500; % hours of videos uploaded (from sources)
 recommended_vids = 10; % number of videos on one's recommended page without scrolling
 avg_vid_length = 12; % average length of a video in minutes (from source)
 
 % Graphing Vectors
-views = zeros(Ndays,1);
-subs = zeros(Ndays,1); % subscribers
+views = zeros(Ndays,1); % views per unit of time
+likes = zeros(Ndays,1); % likes per unit of time
+dislikes = zeros(Ndays,1); % dislikes per unit of time
+comments = zeros(Ndays,1); % comments per unit of time
+subs = zeros(Ndays,1); % subscribers per unit of time
+time = zeros(Ndays,1); % time in days
 
 % Calculated Statistics
 daily_hours = 24*60*60*hours_uploaded_every_minute; % hours of video uploaded a day
@@ -32,49 +40,132 @@ daily_vids = daily_hours/(60/avg_vid_length); % total videos uploaded each day
 
 % probabilities
 recommmend_prob = recommended_vids/daily_vids; % initial chance of viewing a video from reommended
-sub_prob = .0001; % chance of subscribing after viewing (chosen arbitrarily)
-sub_view_prob = .2; % chance of viewing from subscription box (chosen arbitrarily)
+like_ns_prob = .005; % probability of liking a video if not subscribed
+like_sub_prob = .5; % probability of liking a video if subscribed
+dislike_prob = .0025; % probability of disliking a video (will not dislike if subscribed)
+comment_ns_prob = .00125; % probability of commenting on video if not subscribed
+comment_sub_prob = .025; % probability of commenting on video if subscribed
+sub_prob = .075; % chance of subscribing after viewing 
+
+a=1.05;
+b=1.25;
+c=1.5;
+d=2;
+
+
+subscribed = zeros(Nusers,1);
 
 % step through time (assuming one video uploaded a day)
 for i=1:Ndays
-    subscribers = total_subs; % copy this so it doesn't effect loop iteration
-    viewers = Nusers-total_subs; % copy this so it doesn't effect loop iteration
-    viewed = zeros(viewers,1); % logical array indicating whether a video has been viewed or not
-    subbed = 0; % track how many people subscribed today
-    % subscriber case
-    for j=1:total_subs
-        if rand > 1-sub_prob % with a sub_prob percent chance
-            subscribers=subscribers+1; % increment the amount of subscribers
-        end
-    end
-    total_subs=subscribers;
-    % viewer case
-    for j=1:viewers
-        % compute the probability of viewing the video, with the
-        % probability increasing by a factor of 2 to the power of the total
-        % amount of people who have viewed the video
-        p = min(recommmend_prob*2^sum(viewed),1);
-        if rand > 1-p % with a p percent chance
-            viewed(j)=1; % indicate that the video has been viewed
-            if rand > 1-sub_view_prob % with a sub_view_prob chance
-                % gain a subscriber
-                total_subs=total_subs+1; 
-                subbed=subbed+1;
+    time(i)=i; % update time (for graphing)
+    i
+    % stat trackers per unit of time
+    viewed = 0;
+    liked = 0;
+    disliked = 0;
+    commented = 0;
+    subbed = 0;
+
+    % for each user
+    for j=1:Nusers
+        if subscribed(j)
+            viewed = viewed+1;
+            if rand > 1-like_sub_prob
+                liked = liked+1;
+            end
+            if rand > 1-comment_sub_prob
+                commented = commented+1;
             end
         else
-            viewed(j)=0; % otherwise indicate that the video has not been viewed
-        end    
+            max_p = disliked/viewed;
+            if max_p == 0 || isnan(max_p)
+                max_p = 1;
+            end
+            p = min(recommmend_prob*a^viewed*b^liked*c^subbed*d^commented,max_p);
+            if rand > 1-p % with a p percent chance
+                viewed = viewed+1;
+                if rand > 1-sub_prob % with a sub_prob chance
+                    % subscribe
+                    subscribed(j)=1;
+                    subbed=subbed+1;
+                    if rand > 1-like_sub_prob
+                        liked = liked+1;
+                    end
+                    if rand > 1-comment_sub_prob
+                        commented = commented+1;
+                    end
+                else
+                    if rand > 1-like_ns_prob
+                        liked = liked+1;
+                    elseif rand > 1-dislike_prob
+                        disliked = disliked+1;
+                    end
+                    if rand > 1-comment_ns_prob
+                        commented = commented+1;
+                    end
+                end
+            end    
+        end
     end
-    % update total viewcount
-    viewcount = sum(viewed);
-    total_views=total_views+viewcount;
+    subscribed = shuffle(Nusers,subscribed); % randomize order of subscribers 
+                                             % (this is akin to randomizing the order in which users watch the video)
+    total_views=total_views+viewed;
+    total_likes=total_likes+liked;
+    total_dislikes=total_dislikes+disliked;
+    total_comments=total_comments+commented;
+    total_subs=total_subs+subbed;
+    
     % update graphing vectors
-    views(i)=viewcount;
+    views(i)=viewed;
+    likes(i)=liked;
+    dislikes(i)=disliked;
+    comments(i)=commented;
     subs(i)=subbed;
 end
-subs
-views
+p
+figure()
+plot(time,subs, 'r')
+hold on
+xlabel('time')
+ylabel('users')
+title('Channel Growth Over One Year')
+plot(time,views, 'k')
+plot(time,likes, 'b')
+plot(time,comments, 'g')
+plot(time,dislikes, 'y')
+hold off
+
+figure()
+plot(time,subs, 'r')
+hold on
+xlabel('time')
+ylabel('users')
+title('Channel Growth Over One Year')
+plot(time,comments, 'g')
+plot(time,dislikes, 'y')
+
 total_views
 total_subs
+total_likes
+total_dislikes
+total_comments
+
+toc
+end
+
+% This is the Fisher-Yates shuffle algorithm implemented by user Jan on the
+% MATLAB file exchange. This was implemented over simply using randperm
+% because randperm is O(nlog(n)) whereas this shuffle algorithm is O(n).
+% 
+% Jan (2022). Shuffle (https://www.mathworks.com/matlabcentral/fileexchange/27076-shuffle), 
+% MATLAB Central File Exchange. Retrieved April 17, 2022
+function f = shuffle(n,X)
+    for i = 2:n      % Knuth shuffle in forward direction:
+       w    = ceil(rand * i);   % 1 <= w <= i
+       t    = X(w);
+       X(w) = X(i);
+       X(i) = t;
+    end
+    f = X;
 end
 
